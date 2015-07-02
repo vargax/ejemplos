e2112c430a86e428fddeb758cb22639fee9f9c0f
Camilo Vargas Cabrera <vargax@gmail.com>
2015-07-02 09:54:12 -0500

    Haskell nim tras presentación

--- haskell/nim.hs
+++ haskell/nim.hs
@@ -16,15 +16,15 @@ entero2pilas n = sort (digitos n)
                      digitos n = digitos (div n 10) ++ [mod n 10]
 
 -- Construye el entero que representa el juego a partir de las pilas
-pilas2entero' :: Integral a => [a] -> a
-pilas2entero' l = sum $ zipWith (*) potencias $ sort l
+pilas2entero :: Integral a => [a] -> a
+pilas2entero l = sum $ zipWith (*) potencias $ sort l
                where potencias = [10^n | n <- [s-1,s-2..0]]
                      s = length l
                   
-pilas2entero :: Integral a => [a] -> a
-pilas2entero l = suma (sort l)
-               where suma (x:xs) = foldl (\ x y -> 10*x + y) x xs
-
+pilas2entero' :: Integral a => [a] -> a
+pilas2entero' l = suma (sort l)
+                where suma (x:xs) = foldl (\ x y -> 10*x + y) x xs
+ 
 -- Calcula las jugadas posibles para un estado del juego dado
 jugadas :: Integral a => a -> [[a]]
 jugadas n = (mult 0 . aRemover . reverse . entero2pilas) n
@@ -41,4 +41,17 @@ sucesores :: Integral a => a -> [a]
 sucesores n = (limpieza . map ((-)n) . concat . jugadas) n
             where limpieza :: Integral a => [a] -> [a]
                   limpieza = nub . map (pilas2entero . entero2pilas)
+                  
+-- Calcula todas las estrategias posibles para un juego inicial dado
+estrategia :: Integral a => [a] -> [a]
+estrategia [] = []
+estrategia l  = l ++ estrategia (sucesor)
+               where sucesor = 
+                             if sucesores (last l) == []
+                             then []
+                             else [head (sucesores (last l))]
+
+
+
+
           
