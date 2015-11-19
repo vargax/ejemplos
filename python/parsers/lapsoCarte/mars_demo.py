5d42802cbda06bd976a5c9a779ac613891dcd52b
Camilo Vargas Cabrera <vargax@gmail.com>
2015-11-19 13:34:17 +0100

    mars demo parser

---
+++ python/parsers/lapsoCarte/mars_demo.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+import os
+
+INPUT_FILE = 'demo.csv'
+
+output = []
+
+input_file = open(INPUT_FILE)
+for line in input_file:
+    values = line.split(';')
+
+    variable, zone = values.pop(0), values.pop(0)
+    values = [float(x) for x in values]
+
+    t, base = 0, values[0]
+    for data in values:
+        output.append(['Nivel', variable, t, zone, data])
+        output.append(['Base 0', variable, t, zone, data/base])
+        t += 1
+
+try:
+    os.remove('demo.sql')
+except OSError:
+    pass
+
+sql = open('demo.sql', 'w')
+sql.write('DROP TABLE IF EXISTS demo;\n')
+sql.write('CREATE TABLE demo("how" TEXT, "what" TEXT, "when" INT, "where" INT, data FLOAT);\n')
+sql.write('ALTER TABLE demo ADD CONSTRAINT demo_pk PRIMARY KEY("how", "what", "when", "where");\n')
+
+query = "INSERT INTO demo VALUES "
+for row in output:
+    how, what, when, where, data = row
+    query += "('"+how+"','"+what+"',"+str(when)+","+str(where)+","+str(data)+"),"
+
+query = query[:-1] + ';\n'
+sql.write(query)
+sql.close()
