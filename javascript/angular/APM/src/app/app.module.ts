c13c482de0a2a5a8db7c152e966e485d6dfe06b5
Camilo Vargas-Cabrera <vargax@gmail.com>
2021-02-13 05:19:41 -0500

    Pluralsight Angular M7: Interaces, Custom Pipes and Hooks

--- javascript/angular/APM/src/app/app.module.ts
+++ javascript/angular/APM/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ConvertToSpacesPipe
+    ],
   imports: [
     BrowserModule,
     FormsModule
--- javascript/angular/APM/src/app/products/product-list.component.ts
+++ javascript/angular/APM/src/app/products/product-list.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle = 'Product List';
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
-    listFilter  = 'cart';
-    products: any[] = [
+
+    filteredProducts: IProduct[];
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    products: IProduct[] = [
         {
             productId: 2,
             productName: 'Garden Cart',
@@ -33,8 +45,24 @@ export class ProductListComponent {
         }
     ];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart'
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter(
+            (product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+        );
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
     }
 
 }
