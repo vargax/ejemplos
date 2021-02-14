51853d168da53201bf3aec2823c9fbae97ec4860
Camilo Vargas-Cabrera <vargax@gmail.com>
2021-02-14 09:38:59 -0500

    Pluralsight Angular M11: Routing / RouterModule

--- javascript/angular/APM/src/app/app.module.ts
+++ javascript/angular/APM/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ProductListComponent } from './products/product-list.component';
 import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 import {StartComponent} from './shared/start.component';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {ProductDetailComponent} from './products/product-detail.component';
+import {WelcomeComponent} from './home/welcome.component';
 
 @NgModule({
     declarations: [
@@ -18,7 +21,14 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
         BrowserModule,
         FormsModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot([
+            {path: 'products', component: ProductListComponent},
+            {path: 'products/:id', component: ProductDetailComponent},
+            {path: 'welcome', component: WelcomeComponent},
+            {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+            {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+        ])
   ],
   bootstrap: [AppComponent]
 })
--- javascript/angular/APM/src/app/products/product-list.component.ts
+++ javascript/angular/APM/src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import {IProduct} from './product';
 import {ProductService} from './product.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['product-list.component.css']
 })
